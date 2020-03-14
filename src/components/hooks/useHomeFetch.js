d618be44f3da12e6c46d7c6ac3d31957407cbf25
田原 一樹 <ssp0727.lnc@gmail.com>
2020-03-14 18:33:24 +0900

    add searchbar

--- src/components/hooks/useHomeFetch.js
+++ src/components/hooks/useHomeFetch.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { API_KEY, API_URL } from '../../config'
+import { API_KEY, API_URL, POPULAR_BASE_URL } from '../../config'
 
 export const useHomeFetch = () => {
   const [state, setState] = useState({ movies: []})
@@ -32,7 +32,7 @@ export const useHomeFetch = () => {
   }
 
   useEffect(()=> {
-    fetchMovies(`${API_URL}movie/popular?api_key=${API_KEY}`)
+    fetchMovies(POPULAR_BASE_URL)
   },[])
 
   return [{ state, loading, error }, fetchMovies]
