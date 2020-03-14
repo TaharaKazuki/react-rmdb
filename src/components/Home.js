fafd4403f8bd5b4a04d99d0a5aff257d5b964eab
田原 一樹 <ssp0727.lnc@gmail.com>
2020-03-14 11:04:06 +0900

    modify capture

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { IMAGE_BASE_URL, BACKDROP_SIZE } from '../config'
 
 import HeroImage from './elements/HeroImage'
 import SearchBar from './elements/SearchBar'
@@ -12,11 +13,18 @@ import { useHomeFetch } from './hooks/useHomeFetch'
 
 const Home = () => {
   const [{ state, loading, error }, fetchMovies] = useHomeFetch()
-  console.info('state', state)  
+  
+
+  if (error) return <div>Something went wrong ...</div>
+  if (!state.movies[0]) return <Spinner />
 
   return (
     <>
-      <HeroImage />
+      <HeroImage 
+        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
+        title={state.heroImage.original_title}
+        text={state.heroImage.overview}
+      />
       <SearchBar />
       <Grid />
       <MovieThumb />
